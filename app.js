e4111b0a5a80eff30d5f2cb04532d68de0a8277f
umuhoza <didized2001@gmail.com>
2025-11-15 14:04:52 +0200

    Initial Node.js Express setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Web App Jira Demo!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
